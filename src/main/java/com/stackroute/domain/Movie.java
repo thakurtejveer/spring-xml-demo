172428edfb670c9e3a2d3dee0180506ed3d259bd
thakurtejveer <thakurtejveer1@gmail.com>
2019-10-04 12:19:17 +0530

    task 4 completed

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,18 +1,40 @@
 package com.stackroute.domain;
 
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements ApplicationContextAware, BeanNameAware, BeanFactoryAware {
     private Actor actor;
 //    public void setActor(Actor actor) {
 //        this.actor = actor;
 //    }
-
-
     public Movie(Actor actor) {
         this.actor = actor;
     }
 
     public void printActor(){
         actor.studentInfo();
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("BeanFactoryAware implemented");
+
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("BeanNameAware implemented");
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("ApplicationContextAware Implemented");
     }
 }

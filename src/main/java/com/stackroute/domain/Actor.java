81dc9987f7e1cef95003dfc61cb65a21abc14a63
thakurtejveer <thakurtejveer1@gmail.com>
2019-10-04 11:06:05 +0530

    task 3 completed

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -5,29 +5,22 @@ public class Actor {
     private String name;
     private String gender;
     private int age;
-
-    public Actor(String name, String gender, int age) {
-        this.name = name;
-        this.gender = gender;
-        this.age = age;
-    }
     public void studentInfo() {
         System.out.println("Name is : "+name);
         System.out.println("Age is : "+age);
         System.out.println("Gender is : "+gender);
     }
-//    public void setAge(int age) {
-//        this.age = age;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public void setGender(String gender) {
-//        this.gender = gender;
-//    }
-//
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 //    public int getAge() {
 //        return age;
 //    }
